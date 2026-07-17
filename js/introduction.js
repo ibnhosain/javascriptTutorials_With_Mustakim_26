4340d43e430e58b8b1fc2ed28aba8b4d2bced68e
ibnhosain <ibnhosain014@gmail.com>
2026-07-17 22:39:22 +0600

    first commit

---
+++ js/introduction.js
@@ -0,0 +1,13 @@
+/*
+==============What Can JavaScript Do?=================
+1.Change inner html contant
+2.Can Change HTML Styles (CSS)
+3.Can Hide HTML Elements
+
+*/
+//1.Change inner html contant
+document.getElementById("heading_one").innerHTML = " Hi Iam Inner HTML";
+//2.Can Change HTML Styles (CSS)
+document.getElementById("heading_one").style.textAlign = " center";
+//3.Can Hide HTML Elements
+document.getElementById("heading_tow").style.display = " none" ;
