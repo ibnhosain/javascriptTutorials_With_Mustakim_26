0b8c37f120b7aec85e9efff4ef4165d664dfdaca
ibnhosain <ibnhosain014@gmail.com>
2026-07-19 23:14:52 +0600

    variable,datatype

---
+++ js/Variables.js
@@ -0,0 +1,63 @@
+
+{
+// let age = 20;
+// age = 21;
+// age = 22;
+// age = 25;
+
+// const marks = 80;
+// console.log(age, marks);
+}
+
+
+
+
+{
+//  let styling = document.getElementById("lorem");
+//     color = styling;
+//     font = styling;
+//     align = styling;
+
+// color.style.color = "red";
+// font.style.fontSize = "40px";
+// align.style.textAlign= "center";
+
+}
+
+
+{
+// String
+// let color = "Yellow";
+// let lastName = "";
+
+// // Number
+// let length = 16;
+// let weight = 7.5;
+
+
+// // Boolean
+// let x = true;
+// let y = false;
+
+}
+
+
+// let math = "40";
+// let bangla = 60;
+
+// console.log(math + bangla);
+
+
+// let a = 50;
+// let b = 60;
+//  let x = a < b;
+
+//  console.log(x);
+ 
+
+ // let a = "abid";
+
+ // console.log (typeof a);
+
+ // let a = 100;
+ // console.log( typeof a);
