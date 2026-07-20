60c3cfabb40ee91a7894d9da87b3310df8ceca36
ibnhosain <ibnhosain014@gmail.com>
2026-07-20 20:08:48 +0600

    conditionals

---
+++ js/Conditionals.js
@@ -0,0 +1,48 @@
+
+//Conditionals
+
+{
+ let a = 20;
+ //if condition
+ if (a > 18) {
+  document.write("hello , I am condition");
+ }
+
+}
+
+
+//if ....else
+
+{
+ let man = 45;
+
+ if (man > 60) {
+  console.log("Your are rich");
+ } else {
+  console.log("You are not rich");
+ }
+}
+
+//if ...else ...if
+{
+ let man = 40;
+ let isHasCar = true;
+
+if (man > 50 ) {
+  console.log("Your are rich");
+} else if(isHasCar){
+  console.log("Your are rich");
+}else{
+ console.log("You are not rich");
+}
+
+ }
+
+ //ternary (? :)
+
+ {
+  let a = 50;
+  a > 40 ? console.log("you are man"): console.log("you are no man");
+ }
+
+ 
---
+++ js/Operators.js
@@ -0,0 +1,86 @@
+
+//JavaScript Arithmetic
+
+//addition
+
+{
+let a = 10;
+let b = 20;
+let x = a  + b ;
+
+console.log(x);
+
+}
+
+//subtration
+ {
+  let a = 5002
+  let b = 500;
+  let x = a - b;
+  console.log(x);
+ }
+
+
+ //multiplication
+ {
+  let a = 50;
+  let b = 5;
+
+  let x = a * b;
+   
+  console.log(x);
+ }
+
+ 
+ //divition
+
+ {
+  let a = 40 ;
+  let b = 20;
+  let x = a/b;
+  console.log(x);
+ }
+
+
+ // modulus (remainder)
+
+ {
+
+  let a = 53;
+  let b =25;
+  let x = a % b ;
+
+ console.log(x);
+ }
+
+//increment 
+
+{
+ let a = 10;
+ let x =  a++
+
+ console.log(x);
+ console.log(a);
+}
+
+{
+ let a = 10;
+ let x =  a++
+
+ console.log(x);
+ console.log(a);
+}
+
+
+//decrement
+
+{
+ let a = 10;
+ let x = a--
+ console.log(a);
+}
+
+
+
+
+
